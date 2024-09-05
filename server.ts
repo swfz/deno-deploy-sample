391963518c09d58fead2533cc49fc97845befe23
swfz <sawafuji.09@gmail.com>
2024-09-05 09:55:38 +0900

    add log to kv

--- server.ts
+++ server.ts
@@ -2,6 +2,20 @@ import { ulid } from "jsr:@std/ulid";
 
 const kv = await Deno.openKv();
 
+const logObject = async (now: Date, req: Request) => {
+  const ts = Math.floor(now.getTime() / 1000);
+
+  return {
+    method: req.method,
+    url: req.url,
+    redirect: req.redirect,
+    bodyUsed: req.bodyUsed,
+    ...{ts: ts},
+    headers: Object.fromEntries(req.headers.entries()),
+    ...(req.body ? { body: await req.text() } : {})
+  }
+}
+
 Deno.serve(async (request: Request) => {
   // Create short links
   if (request.method == "POST") {
@@ -15,9 +29,10 @@ Deno.serve(async (request: Request) => {
   const slug = request.url.split("/").pop() || "";
   const url = (await kv.get(["links", slug])).value as string;
 
-  // logging
+  // logging cache 90 days
   const now = new Date()
-  await kv.set(["logs", now.getFullYear(), now.getMonth(), now.getDate(), ulid()], request, { expireIn: 1000 * 60 * 60 * 24 * 30 });
+  const logRecord = await logObject(now, request);
+  await kv.set(["logs", now.getFullYear(), now.getMonth() + 1, now.getDate(), ulid()], logRecord, { expireIn: 1000 * 60 * 60 * 24 * 90 });
 
   if (url) {
     return Response.redirect(url, 301);
